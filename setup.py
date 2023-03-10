b7803c24ed05baf65f695745aae58ec008a5f024
Anupam Singh <64646798+AnupamAS0x1@users.noreply.github.com>
2023-03-10 10:33:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools.command.install import install
 print("test anupamas01 ")
 
 
-setup(name='specaiseg',
+setup(name='fext-cli',
       version='1.1.1',
       description='AnupamAS01',
       author='AnupamAS01',
