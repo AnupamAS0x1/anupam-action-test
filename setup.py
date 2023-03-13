f0dc169b4ab83d5fe05a8fe72e71bb1211db67eb
Anupam Singh <64646798+AnupamAS0x1@users.noreply.github.com>
2023-03-13 13:55:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools.command.install import install
 print("test anupamas01 ")
 
 
-setup(name='termcolour',
-      version='10.1.1',
+setup(name='django-microservices-admin',
+      version='0.0.1',
       description='AnupamAS01',
       author='AnupamAS01',
       license='MIT',
